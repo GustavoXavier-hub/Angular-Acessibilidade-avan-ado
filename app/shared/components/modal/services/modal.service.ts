df9dea06ea0afc642e8832087415b58ab0be5b18
Gustavo Xavier <gus.guilhermexavier08@gmail.com>
2022-11-02 18:33:20 -0300

    interface API

--- app/shared/components/modal/services/modal.service.ts
+++ app/shared/components/modal/services/modal.service.ts
@@ -1,3 +1,21 @@
-import { Injectable } from "@angular/core";
+import { Injectable, TemplateRef } from "@angular/core";
 @Injectable()
-export class ModalService {}
+export class ModalService {
+
+public open(config: ModalConfig) {
+ console.log('open called');
+ return new ModalRef();
+    }
+}
+
+export interface ModalConfig{
+ templateRef: TemplateRef<any>;
+ title: string;   
+}
+
+export class ModalRef {
+  
+    public close(): void {
+     console.log('close called')   
+    }
+}
